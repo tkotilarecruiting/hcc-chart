b3eb6601443ca9ae92bb4c1d4411b4d360abd14f
tkotilarecruiting <tkotilarecruiting@gmail.com>
2026-09-11 09:55:31 -0500

    Deploy charting app update; SW cache v13

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //   • Static files are cache-first so they load offline too.
 //
 // The cache version is bumped automatically on each deploy (deploy_chart.py).
-const CACHE = "hcc-chart-v12";
+const CACHE = "hcc-chart-v13";
 const SHELL = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
